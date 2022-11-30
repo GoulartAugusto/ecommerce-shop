45c468b9fd2f23b888f79d720e004c167c651005
GoulartAugusto <goulart.augusto@gmail.com>
2022-11-30 18:29:51 -0300

    creating an admin dashboard

---
+++ src/admin/AddProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+
+const AddProducts = () => {
+  return (
+    <div>
+        AddProduct
+    </div>
+  )
+}
+
+export default AddProducts
